81ae216351a96374fdb8231c916bd3bc071f4a94
Hanna Söderström <gothbarbie84@gmail.com>
2022-02-13 12:22:16 +0100

    feat: add Holiday interface

--- sweden2022.ts
+++ sweden2022.ts
@@ -1,3 +1,17 @@
+export interface Holiday {
+  name: string;
+  name_local: string;
+  language: string;
+  country: string;
+  location: string;
+  type: string;
+  date: string;
+  date_year: string;
+  date_month: string;
+  date_day: string;
+  week_day: string;
+}
+
 export const sweden2022 = [
   {
     name: "New Year's Day",
